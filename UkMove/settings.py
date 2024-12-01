1f5d97204248b627a16449f34b7ae34a8b3e9712
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-12-01 23:35:53 +0000

    Add post model to blog app

--- UkMove/settings.py
+++ UkMove/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&w)6e_gleire1qdbsz(lpd$bz^1v*cm0gq6=-%0lxhg$jalvr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-techistem-ukmovecip4-yumdm2ubfse.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -90,6 +90,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
