5fb5e91441813e7098aa2c84a251d9730fcba0d0
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-12-03 23:38:40 +0000

    generic post view

--- UkMove/urls.py
+++ UkMove/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.my_blog, name="blog"),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
