5a68d35e616d8fa8387445dd59a1df2ba08a8ea5
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-12-04 14:34:13 +0000

    Add base template and blog template

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,7 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
     
-    # template_name = "post_list.html"
+    
