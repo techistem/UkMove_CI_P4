cc048d003ba9071587422d8dedf6c9c8521a26a0
Gulten Istem Karakas <istem.karakas@gmail.com>
2024-12-16 11:47:57 +0000

    Change SECRET_KEY

--- UkMove/settings.py
+++ UkMove/settings.py
@@ -26,11 +26,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&w)6e_gleire1qdbsz(lpd$bz^1v*cm0gq6=-%0lxhg$jalvr5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-techistem-ukmovecip4-yumdm2ubfse.ws.codeinstitute-ide.net',
     '.herokuapp.com']
